a6f2a6283b8869d24e6f7afc338f45d67e29a822
dell <dell@dell-PC>
2017-06-13 23:28:00 +0530

    jdbc

---
+++ src/main/java/ss/jdk/TestConnectionUtil.java
@@ -0,0 +1,11 @@
+package ss.jdk;
+
+import java.sql.SQLException;
+
+public class TestConnectionUtil {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		System.out.println(ConnectionUtil.getConnection());
+	}
+
+}
